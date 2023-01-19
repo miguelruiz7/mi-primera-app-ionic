3cc34261542b6c58ee72b8bd258d17ce4a9dc346
miguelruiz7 <miguelruizzamora7@gmail.com>
2023-01-19 16:34:07 -0600

    Mi primer respaldo

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
   constructor() {}
+
+  ngOnInit(): void {
+    console.log("Hola mundo");
+    let numero = 20;
+    //debugger
+    numero = 10;
+    numero = 15;
+    numero = 100;
+
+    console.log("La variable está tomando este valor;" +numero);
+  }
 
 }
